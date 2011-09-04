618f73dce4c535571ab8a39a2144e961445907a1
Washington Botelho <wbotelhos@gmail.com>
2011-09-04 19:17:08 -0300

    O método .values() já existe para esse objetivo.

--- src/br/com/wbotelhos/model/common/TipoPerfil.java
+++ src/br/com/wbotelhos/model/common/TipoPerfil.java
@@ -1,8 +1,5 @@
 package br.com.wbotelhos.model.common;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 /**
  * @author Washington Botelho
  * @artigo http://wbotelhos.com.br/2010/04/23/controle-de-permissao-com-vraptor-3
@@ -11,15 +8,5 @@ import java.util.Collection;
 public enum TipoPerfil {
 
 	MEMBRO, MODERADOR, ADMINISTRADOR;
-
-	public static Collection<TipoPerfil> loadAll() {
-		Collection<TipoPerfil> perfis = new ArrayList<TipoPerfil>();
-
-		for (TipoPerfil perfil : values()) {
-			perfis.add(perfil);
-		}
-
-		return perfis;
-	}
 
 }
